6b7e5ffaf81929e7e0f82ab91170b7eace5b7dc7
Yongye Tan <yongyetan1209@gmail.com>
2024-09-22 01:26:34 -0400

    add model container to main app entry

--- EasiCash/EasiCashApp.swift
+++ EasiCash/EasiCashApp.swift
@@ -6,14 +6,24 @@
 //
 
 import SwiftUI
-import TipKit
+import SwiftData
 
 @main
 struct EasiCashApp: App {
+
+    let container: ModelContainer = {
+        let schema = Schema([MenuItem.self, CheckOutList.self])
+
+        // swiftlint:disable:next force_try
+        let container = try! ModelContainer(for: schema, configurations: [])
+        return container
+    }()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .previewableTip()
+                .modelContainer(container)
         }
     }
 }
